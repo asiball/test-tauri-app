7d898aed2d424b5bd86d740bec860c428b7573bb
asiball <163713784+asiball@users.noreply.github.com>
2025-09-17 18:34:46 +0900

    feat: Update configuration and structure for improved linting and formatting

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,26 @@
 import eslint from "@eslint/js";
-import tseslint from "typescript-eslint";
+import { defineConfig, globalIgnores } from "eslint/config";
 import globals from "globals";
+import tseslint from "typescript-eslint";
 
-export default tseslint.config(
-    {
-        ignores: ["public/**", "release/**", "build/**", "dist/**", "node_modules/**"],
-        languageOptions: {
-            globals: {
-                ...globals.browser,
-                ...globals.node,
-            },
-        },
-
+export default defineConfig(
+  globalIgnores([
+    "public/**",
+    "release/**",
+    "build/**",
+    "dist/**",
+    "node_modules/**",
+    "src-tauri/**",
+  ]),
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
     },
-    eslint.configs.recommended,
-    tseslint.configs.strict,
-    tseslint.configs.stylistic
+  },
+  eslint.configs.recommended,
+  tseslint.configs.strict,
+  tseslint.configs.stylistic
 );
